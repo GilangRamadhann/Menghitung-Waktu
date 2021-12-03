d4f8837b06dfb1939c428fb4a4eba3cbc47a0c41
Gilang Ramadhan <92220215+GilangRamadhann@users.noreply.github.com>
2021-12-03 16:02:52 +0700

    Add files via upload

---
+++ Hari Membosankan.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	system ("color 0A");
+
+	int detik,jam,menit,total,sjam,smenit;
+
+ 	cout<<"Masukkan Jumlah Detik : ";
+	
+  	kemacetan = jumlah/3600; 
+   	jam = total%3600; 
+   	menit = sjam/60; 
+	menit = sjam%60; 
+   	detik = smenit;
+   	cout<<jam<<" Jam "<<menit<<" Menit "<<detik<<" Detik";
+	
+	return 0;
+}
+
